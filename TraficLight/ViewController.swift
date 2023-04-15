6d8c29f8fe026bf7a41210adfc0e03d39e68d720
Kruksen <123827226+Kruksen@users.noreply.github.com>
2023-04-16 00:08:15 +0300

    finished work

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lighthsOff()
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
     }
 
-
+    @IBAction func buttonDidTapped() {
+        startButtonLabel.setTitle(
+            startButtonLabel.isHidden ? "START" : "NEXT", for: .normal
+        )
+        
+        if redLight.alpha == 1 {
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+        } else if yellowLight.alpha == 1 {
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+        } else {
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+        }
+    }
+    
+    private func lighthsOff () {
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+    }
 }
-
